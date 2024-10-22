a3bf55224bab1b86a6318b87656c2059a9c9cab5
Anish Vijapura <anish.9311@gmail.com>
2024-10-22 17:04:56 +0530

    Configured docker compose and checks to db

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+calculator function
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract a from y and return"""
+    return y - x
+    
